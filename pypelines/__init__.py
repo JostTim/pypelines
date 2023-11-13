08721d70df380a205b9271954b278ad93f5910ef
Timothe Jost <timothe.jost@wanadoo.fr>
2023-11-13 19:09:22 +0100

    publish

--- pypelines/__init__.py
+++ pypelines/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .pipes import *
 from .pipelines import *
 from .steps import *
 from .disk import *
 from .sessions import *
-#from .versions import *
+
+# from .versions import *
