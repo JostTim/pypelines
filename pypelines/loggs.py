8a4126e45bf919ca70501c62b100622722afc5b3
Timothe Jost <timothe.jost@wanadoo.fr>
2023-12-14 12:45:30 +0100

    logging with colors

--- pypelines/loggs.py
+++ pypelines/loggs.py
@@ -1,5 +1,90 @@
 import logging
 from functools import wraps
+import coloredlogs
+
+
+def enable_logging(level="DEBUG", file_level="INFO"):
+    import logging, os, sys
+
+    # Create a logger object.
+    logger = logging.getLogger()  # root logger
+
+    # make sure we start fresh
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+    # Create a filehandler object for file
+    fh = logging.FileHandler("test.log")
+    fh.setLevel(file_level)
+    fh.setFormatter(FileFormatter())
+
+    # Create a filehandler object for terminal
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(level)
+    ch.setFormatter(TerminalFormatter())
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+
+NAMELENGTH = 33
+LEVELLENGTH = 10
+
+
+class TerminalFormatter(coloredlogs.ColoredFormatter):
+    FORMAT = f"%(levelname)-{LEVELLENGTH}s : %(name)-{NAMELENGTH}s : %(message)s  -  %(asctime)s"
+
+    def __init__(self, fmt=FORMAT, datefmt=None, style="%"):
+        super().__init__(
+            fmt=fmt,
+            datefmt=datefmt,
+            style=style,
+            level_styles={
+                "critical": {"bold": True, "color": "red"},
+                "debug": {},
+                "error": {"color": "red"},
+                "info": {"color": "blue"},
+                "warning": {"color": "yellow"},
+            },
+            field_styles={
+                "asctime": {"color": "green"},
+                "hostname": {"color": "magenta"},
+                #'levelname': {'bold': True, 'color': 'black'},
+                "name": {"color": "blue"},
+            },
+        )
+
+
+class FileFormatter(coloredlogs.ColoredFormatter):
+    FORMAT = f"[%(asctime)s] - %(levelname)-{LEVELLENGTH}s : %(name)-{NAMELENGTH}s : %(message)s"
+
+    def __init__(self, fmt=FORMAT, datefmt=None, style="%"):
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+
+def set_handler_(logger, worker_pk, level="INFO"):
+    fh = logging.FileHandler(f"{worker_pk}.log")
+    fh.setLevel(level)
+    fh.setFormatter(FileFormatter())
+    logger.addHandler(fh)
+
+
+def remove_handler(logger):
+    logger.removeHandler(logger.handlers[-1])
+
+
+class LogToLocalFile:
+    def __init__(self, logger_name, worker_pk):
+        self.logger_name = logger_name
+        self.worker_pk = worker_pk
+
+    def __enter__(self):
+        self.logger = logging.getLogger(self.logger_name)
+        set_logger_formatter(self.logger, self.worker_pk)
+        return self.logger
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        remove_logger_handler(self.logger)
 
 
 class ContextFilter(logging.Filter):
